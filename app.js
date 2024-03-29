f7402605b8d400ca7bee8062fc128cf6acab4897
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-03-28 23:05:00 -0300

    Exerciciio 2

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
+
+
+app.get("/sobre", function (req, res){
+    res.send("O aplicativo funciona")
+
+})
+
+app.get("/contato", function (req, res){
+    res.send("Contato cadastrado com sucesso!")
+
+})
+
+
 
 app.listen(3000, function(){
     console.log("Servidor Rodando")
